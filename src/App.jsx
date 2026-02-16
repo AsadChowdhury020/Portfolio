581e8c039143871d339439694d8ce1e7785654bf
Your NameAsadChowdhury020 <asadchowdhury020@gmail.com>
2026-02-16 12:25:23 +0600

    After completeing the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import Navbar from "./Layouts/Navbar"
+import About from "./Sections/About"
+import Contact from "./Sections/Contact"
+import Experience from "./Sections/Experience"
+import Hero from "./Sections/Hero"
+import Projects from "./Sections/Projects"
+import Testimonials from "./Sections/Testimonials"
 
 function App() {
-
   return (
-    <div>
+    <div className="min-h-screen overflow-x-hidden ">
 
+      <Navbar />
+
+      {/* <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main> */}
     </div>
   )
 }
---
+++ src/Sections/Experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <section>
+            Experiences
+        </section>
+    );
+};
+
+export default Experience;
